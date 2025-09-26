13f4f4a673faf860b72b9ac85d57bcd930644f9a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-09-26 23:57:06 +0600

    Footer Related Style Update

--- src/Components/Footer/FooterBody.jsx
+++ src/Components/Footer/FooterBody.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 const FooterBody = () => {
     return (
         <div className='md:mt-20 mt-5 md:p-20 p-5 grid md:grid-cols-6 grid-cols-1 gap-10 md:gap-0'>
-            <div className='col-span-2 text-left pr-15'>
+            <div className='col-span-2 md:text-left text-justify md:pr-15'>
                 <h3 className='text-white md:mb-4 mb-2'>CS — Ticket System</h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </div>
